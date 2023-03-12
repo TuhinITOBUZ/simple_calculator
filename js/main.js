c8b28306b6491f68313c2e9cd121bc2c75c99ace
Tuhin Mukherjee <tuhinmuherjee74@gmail.com>
2023-03-12 19:31:46 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,63 @@
+let number1 = "";
+let number2 = "";
+let operation = "";
+
+document.querySelectorAll(".number-button").forEach((element) => {
+  element.addEventListener("click", () => {
+    let inputValue = document.getElementById("user-input").value;
+    document.getElementById("user-input").value =
+      inputValue + element.textContent.toString();
+  });
+});
+
+document.querySelectorAll(".special-button").forEach((element) => {
+  element.addEventListener("click", () => {
+    if (element.textContent === "AC") {
+      document.getElementById("user-input").value = "";
+      document.getElementById("user-input").placeholder = "0";
+      operation = "";
+      number1 = "";
+      number2 = "";
+    } 
+    else if (element.textContent === "Clear"){
+        let number = document.getElementById('user-input').value
+        document.getElementById('user-input').value = number.substring(0,number.length-1)
+    }
+    else if (element.textContent === "x") {
+      operation = element.textContent.toString();
+      number1 = document.getElementById("user-input").value;
+      document.getElementById("user-input").placeholder = number1;
+      document.getElementById("user-input").value = "";
+    } else if (element.textContent === "+") {
+      operation = element.textContent.toString();
+      number1 = document.getElementById("user-input").value;
+      document.getElementById("user-input").placeholder = number1;
+      document.getElementById("user-input").value = "";
+    } else if (element.textContent === "-") {
+      operation = element.textContent.toString();
+      number1 = document.getElementById("user-input").value;
+      document.getElementById("user-input").placeholder = number1;
+      document.getElementById("user-input").value = "";
+    } else if (element.textContent === "/") {
+      operation = element.textContent.toString();
+      number1 = document.getElementById("user-input").value;
+      document.getElementById("user-input").placeholder = number1;
+      document.getElementById("user-input").value = "";
+    } else if (element.textContent === "=") {
+      number2 = document.getElementById("user-input").value;
+      if (operation === "+") {
+        document.getElementById("user-input").value =
+          Number(number1) + Number(number2);
+      } else if (operation === "-") {
+        document.getElementById("user-input").value =
+          Number(number1) - Number(number2);
+      } else if (operation === "x") {
+        document.getElementById("user-input").value =
+          Number(number1) * Number(number2);
+      } else if (operation === "/") {
+        document.getElementById("user-input").value =
+          Number(number1) / Number(number2);
+      }
+    }
+  });
+});
