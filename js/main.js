b02ef294b7056e864af40e91a8eb23c8eb1a9b37
IT012307 <it012307@MacBooks-MacBook-Pro.local>
2023-03-13 14:35:51 +0530

    Final commit

--- js/main.js
+++ js/main.js
@@ -2,68 +2,190 @@ let number1 = "";
 let number2 = "";
 let operation = "";
 
+function clear() {
+  document.getElementById("user-input").value = "";
+  document.getElementById("user-input").placeholder = "0";
+  document.getElementById("user-input-history").value = "";
+  number1 = "";
+  operation = "";
+  number2 = "";
+}
+
+function performOperation(number1, operation, number2) {
+  let result = 0;
+  if (operation === "+") {
+    result = Number(number1) + Number(number2);
+  } else if (operation === "-") {
+    result = Number(number1) - Number(number2);
+  } else if (operation === "x") {
+    result = Number(number1) * Number(number2);
+  } else if (operation === "/") {
+    result = Number(number1) / Number(number2);
+  }
+  clear();
+  return result;
+}
+
+function addValueInInputField(element) {
+  let inputValue = document.getElementById("user-input").value;
+  document.getElementById("user-input").value =
+    inputValue + element.textContent.toString();
+}
+
+function changeOperation(element) {
+  operation = element.textContent.toString();
+  document.getElementById("user-input-history").value = number1 + operation;
+}
+
+function number1ValueAddition(element) {
+  if (document.getElementById("user-input").value != "") {
+    operation = element.textContent.toString();
+    number1 = document.getElementById("user-input").value;
+    document.getElementById("user-input").placeholder = number1;
+    document.getElementById("user-input-history").value = number1 + operation;
+    document.getElementById("user-input").value = "";
+  } else {
+    alert("No Value given!");
+  }
+}
+
+function setUserInput(){
+  document.getElementById("user-input").value = "";
+  document.getElementById("user-input").placeholder = "0";
+}
+
 document.querySelectorAll(".number-button").forEach((element) => {
   element.addEventListener("click", () => {
-    let inputValue = document.getElementById("user-input").value;
-    document.getElementById("user-input").value =
-      inputValue + element.textContent.toString();
+    addValueInInputField(element);
   });
 });
 
 document.querySelectorAll(".special-button").forEach((element) => {
   element.addEventListener("click", () => {
     if (element.textContent === "AC") {
-      document.getElementById("user-input").value = null;
-      document.getElementById("user-input").placeholder = null;
-      document.getElementById("user-input-history").value = null;
-      operation = "";
-      number1 = "";
-      number2 = "";
-    } else if (element.textContent === "Clear") {
+      clear();
+    } else if (element.textContent === "del") {
       let number = document.getElementById("user-input").value;
       document.getElementById("user-input").value = number.substring(
         0,
         number.length - 1
       );
+    } else if (element.textContent === ".") {
+      let inputArr = document.getElementById("user-input").value.split(".");
+      if (inputArr.length == 1) {
+        addValueInInputField(element);
+      }
     } else if (element.textContent === "x") {
-      operation = element.textContent.toString();
-      number1 = document.getElementById("user-input").value;
-      document.getElementById("user-input").placeholder = number1;
-      document.getElementById("user-input-history").value = number1 + operation;
-      document.getElementById("user-input").value = "";
+      if (number1 === "") {
+        number1ValueAddition(element);
+      } else if (
+        operation != "" &&
+        number1 != "" &&
+        document.getElementById("user-input").value === ""
+      ) {
+        changeOperation(element);
+      } else if (
+        operation != "" &&
+        number1 != "" &&
+        document.getElementById("user-input").value != ""
+      ) {
+        number2 = document.getElementById("user-input").value;
+        number1 = performOperation(number1, operation, number2);
+        changeOperation(element);
+        setUserInput()
+      } else {
+        number2 = document.getElementById("user-input").value;
+        number1 = performOperation(number1, "*", number2)
+        operation = element.textContent.toString();
+        document.getElementById("user-input-history").value =
+          number1 + operation;
+        setUserInput()
+      }
     } else if (element.textContent === "+") {
-      operation = element.textContent.toString();
-      number1 = document.getElementById("user-input").value;
-      document.getElementById("user-input").placeholder = number1;
-      document.getElementById("user-input-history").value = number1 + operation;
-      document.getElementById("user-input").value = "";
+      if (number1 === " ") {
+        number1ValueAddition(element);
+      } else if (
+        operation != "" &&
+        number1 != "" &&
+        document.getElementById("user-input").value === ""
+      ) {
+        changeOperation(element);
+      } else if (
+        operation != "" &&
+        number1 != "" &&
+        document.getElementById("user-input").value != ""
+      ) {
+        number2 = document.getElementById("user-input").value;
+        number1 = performOperation(number1, operation, number2);
+        changeOperation(element);
+        setUserInput()
+      } else {
+        number2 = document.getElementById("user-input").value;
+        number1 = performOperation(number1, "+", number2)
+        operation = element.textContent.toString();
+        document.getElementById("user-input-history").value =
+          number1 + operation;
+        setUserInput()
+      }
     } else if (element.textContent === "-") {
-      operation = element.textContent.toString();
-      number1 = document.getElementById("user-input").value;
-      document.getElementById("user-input").placeholder = number1;
-      document.getElementById("user-input-history").value = number1 + operation;
-      document.getElementById("user-input").value = "";
+      if (number1 === "") {
+        number1ValueAddition(element);
+      } else if (
+        operation != "" &&
+        number1 != "" &&
+        document.getElementById("user-input").value === ""
+      ) {
+        changeOperation(element);
+      } else if (
+        operation != "" &&
+        number1 != "" &&
+        document.getElementById("user-input").value != ""
+      ) {
+        number2 = document.getElementById("user-input").value;
+        number1 = performOperation(number1, operation, number2);
+        changeOperation(element);
+        setUserInput()
+      } else {
+        number2 = document.getElementById("user-input").value;
+        number1 = performOperation(number1, "-", number2)
+        operation = element.textContent.toString();
+        document.getElementById("user-input-history").value =
+          number1 + operation;
+        setUserInput()
+      }
     } else if (element.textContent === "/") {
-      operation = element.textContent.toString();
-      number1 = document.getElementById("user-input").value;
-      document.getElementById("user-input").placeholder = number1;
-      document.getElementById("user-input-history").value = number1 + operation;
-      document.getElementById("user-input").value = "";
+      if (number1 === "") {
+        number1ValueAddition(element);
+      } else if (
+        operation != "" &&
+        number1 != "" &&
+        document.getElementById("user-input").value === ""
+      ) {
+        changeOperation(element);
+      } else if (
+        operation != "" &&
+        number1 != "" &&
+        document.getElementById("user-input").value != ""
+      ) {
+        number2 = document.getElementById("user-input").value;
+        number1 = performOperation(number1, operation, number2);
+        changeOperation(element);
+        setUserInput()
+      } else {
+        number2 = document.getElementById("user-input").value;
+        number1 = performOperation(number1, "/", number2)
+        operation = element.textContent.toString();
+        document.getElementById("user-input-history").value =
+          number1 + operation;
+        setUserInput()
+      }
     } else if (element.textContent === "=") {
       number2 = document.getElementById("user-input").value;
-      if (operation === "+") {
-        document.getElementById("user-input").value =
-          Number(number1) + Number(number2);
-      } else if (operation === "-") {
-        document.getElementById("user-input").value =
-          Number(number1) - Number(number2);
-      } else if (operation === "x") {
-        document.getElementById("user-input").value =
-          Number(number1) * Number(number2);
-      } else if (operation === "/") {
-        document.getElementById("user-input").value =
-          Number(number1) / Number(number2);
-      }
+      document.getElementById("user-input").value = result = performOperation(
+        number1,
+        operation,
+        number2
+      );
     }
   });
 });
