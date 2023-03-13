4f418d2e5e07cede66304e8b2bae3221b63811d9
IT012307 <it012307@MacBooks-MacBook-Pro.local>
2023-03-13 11:54:43 +0530

    added history input box

--- js/main.js
+++ js/main.js
@@ -13,35 +13,41 @@ document.querySelectorAll(".number-button").forEach((element) => {
 document.querySelectorAll(".special-button").forEach((element) => {
   element.addEventListener("click", () => {
     if (element.textContent === "AC") {
-      document.getElementById("user-input").value = "";
-      document.getElementById("user-input").placeholder = "0";
+      document.getElementById("user-input").value = null;
+      document.getElementById("user-input").placeholder = null;
+      document.getElementById("user-input-history").value = null;
       operation = "";
       number1 = "";
       number2 = "";
-    } 
-    else if (element.textContent === "Clear"){
-        let number = document.getElementById('user-input').value
-        document.getElementById('user-input').value = number.substring(0,number.length-1)
-    }
-    else if (element.textContent === "x") {
+    } else if (element.textContent === "Clear") {
+      let number = document.getElementById("user-input").value;
+      document.getElementById("user-input").value = number.substring(
+        0,
+        number.length - 1
+      );
+    } else if (element.textContent === "x") {
       operation = element.textContent.toString();
       number1 = document.getElementById("user-input").value;
       document.getElementById("user-input").placeholder = number1;
+      document.getElementById("user-input-history").value = number1 + operation;
       document.getElementById("user-input").value = "";
     } else if (element.textContent === "+") {
       operation = element.textContent.toString();
       number1 = document.getElementById("user-input").value;
       document.getElementById("user-input").placeholder = number1;
+      document.getElementById("user-input-history").value = number1 + operation;
       document.getElementById("user-input").value = "";
     } else if (element.textContent === "-") {
       operation = element.textContent.toString();
       number1 = document.getElementById("user-input").value;
       document.getElementById("user-input").placeholder = number1;
+      document.getElementById("user-input-history").value = number1 + operation;
       document.getElementById("user-input").value = "";
     } else if (element.textContent === "/") {
       operation = element.textContent.toString();
       number1 = document.getElementById("user-input").value;
       document.getElementById("user-input").placeholder = number1;
+      document.getElementById("user-input-history").value = number1 + operation;
       document.getElementById("user-input").value = "";
     } else if (element.textContent === "=") {
       number2 = document.getElementById("user-input").value;
